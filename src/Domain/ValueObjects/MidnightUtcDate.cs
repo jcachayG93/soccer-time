67511363671e311b59316e61dfedea223a4a8181
Julio Cachay <jcachay@mktg33.com>
2024-01-06 07:15:38 -0500

    Added a value object

---
+++ src/Domain/ValueObjects/MidnightUtcDate.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Exceptions;
+
+namespace Domain.ValueObjects;
+
+/// <summary>
+/// Wraps a DateTime object, enforcing UTC formatting and settings the time to the
+/// start of the day (12 am: Midnight)
+/// </summary>
+public record MidnightUtcDate
+{
+    public DateTime Value { get; }
+    
+    public MidnightUtcDate(DateTime value)
+    {
+        // https://stackoverflow.com/a/13467254/14132160
+        Value = value.Date;
+        
+        if (Value.Kind != DateTimeKind.Utc)
+        {
+            throw new InvalidEntityStateException("Date value must be UTC.");
+        }
+    }
+}
---
+++ testing/Domain.UnitTests/ValueObjects/MidnightUtcDateTests.cs
@@ -0,0 +1,59 @@
+﻿using Domain.Exceptions;
+using Domain.ValueObjects;
+
+namespace Domain.UnitTests.ValueObjects;
+
+public class MidnightUtcDateTests
+{
+    [Theory]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    public void AssertsValueIsUtc(
+        bool isUtc, bool shouldThrow)
+    {
+        // ************ ARRANGE ************
+
+        var value = isUtc ? DateTime.UtcNow : DateTime.Now;
+        
+        // ************ ACT ****************
+
+        var result = Record.Exception(() =>
+            new MidnightUtcDate(value));
+        
+        // ************ ASSERT *************
+
+        if (shouldThrow)
+        {
+            Assert.NotNull(result);
+            Assert.IsType<InvalidEntityStateException>(result);
+            Assert.Equal("Date value must be UTC.", result.Message);
+        }
+        else
+        {
+            Assert.Null(result);
+        }
+    }
+
+    [Fact]
+    public void ValueIsInputStartingAtMidnight()
+    {
+        // ************ ARRANGE ************
+
+        var value = DateTime.UtcNow; 
+
+        // ************ ACT ****************
+
+        var result = new MidnightUtcDate(value);
+
+        // ************ ASSERT *************
+        
+        Assert.Equal(value.Year, result.Value.Year);
+        Assert.Equal(value.Month, result.Value.Month);
+        Assert.Equal(value.Day, result.Value.Day);
+        
+        Assert.Equal(0, result.Value.Hour);
+        Assert.Equal(0, result.Value.Minute);
+        Assert.Equal(0, result.Value.Second);
+        Assert.Equal(0, result.Value.Millisecond);
+    }
+}
